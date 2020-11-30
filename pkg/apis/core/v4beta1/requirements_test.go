43a15e105438b2f9f88b294a2fd38b5b2cad6007
James Rawlings <rawlingsj80@gmail.com>
2020-11-30 10:46:23 +0000

    fix: schema generation so we get embedded structs
    use a fork until https://github.com/alecthomas/jsonschema/issues/65 is fixed

--- pkg/apis/core/v4beta1/requirements_test.go
+++ pkg/apis/core/v4beta1/requirements_test.go
@@ -572,8 +572,9 @@ func TestBackwardsCompatibleRequirementsFile(t *testing.T) {
 
 func validateRequirements(t *testing.T, oldRequirementsDir string) {
 	requirementsResource, fileName, err := v4beta1.LoadRequirementsConfig(oldRequirementsDir, true)
-	requirements := &requirementsResource.Spec
+
 	assert.NoError(t, err, "failed to load old style jx-requirements.yml")
+	requirements := &requirementsResource.Spec
 	assert.NotEmpty(t, fileName, "requirements filename should not be empty")
 	assert.NotNil(t, requirements, "requirements should not be empty")
 	assert.Equal(t, v4beta1.WebhookTypeLighthouse, requirements.Webhook, "failed to find requirement")
